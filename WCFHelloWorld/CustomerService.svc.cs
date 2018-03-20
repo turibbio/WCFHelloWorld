9ff65b12b31cf86ed6abbc39bd38d22bf89ba5c0
Antonio Liccardi <antonio.liccardi@blexin.com>
2018-03-20 20:44:51 +0100

    singleton

--- WCFHelloWorld/CustomerService.svc.cs
+++ WCFHelloWorld/CustomerService.svc.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace WCFHelloWorld
 {
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class CustomerService : ICustomerService
     {
         List<Customer> customerList = new List<Customer> {
