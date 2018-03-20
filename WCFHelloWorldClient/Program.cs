a04bed862f25b63d2dcd0c090ef6ece859cc0ff0
Antonio Liccardi <antonio.liccardi@blexin.com>
2018-03-20 12:40:34 +0100

    adding a client

---
+++ WCFHelloWorldClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFHelloWorldClient.CustomerServiceReference;
+
+namespace WCFHelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerServiceClient client = new CustomerServiceClient();
+            var customers = client.GetCustomers();
+        }
+    }
+}
